899cc90dac5f58e57cf8e379e8425bb44cd4fb1d
kuehacker <36141806+kuehacker@users.noreply.github.com>
2018-07-01 11:05:51 +0700

    Update dd.py

--- dd.py
+++ dd.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+================================== ============================= #
 from akad.ttypes import *
 from multiprocessing import Pool, Process
 from datetime import datetime
